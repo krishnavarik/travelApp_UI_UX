a99083d210bf96585291b07583fa4b6aebd55a1f
krishnavarik <krishnavarik9@gmail.com>
2024-05-05 00:49:21 +0530

    second commit

---
+++ components/Footer.tsx
@@ -0,0 +1,84 @@
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  let currentYear = new Date().getFullYear()
+  return (
+    <footer className="flexCenter mb-24">
+      <div className="padding-container max-container flex w-full flex-col gap-14">
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
+          <Link href="/" className="mb-10">
+            <Image src="/hilink-logo.svg" alt="logo" width={74} height={24} />
+          </Link>
+          <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
+            {FOOTER_LINKS.map((columns) => (
+              <FooterComp title={columns.title}>
+                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
+                  {columns.links.map((link) => {
+                    return (
+                      <Link href="/" key={link}>
+                        {link}
+                      </Link>
+                    );
+                  })}
+                </ul>
+              </FooterComp>
+            ))}
+
+            <div className="flex flex-col gap-5">
+              <FooterComp title={FOOTER_CONTACT_INFO.title}>
+                {FOOTER_CONTACT_INFO.links.map((link) => {
+                  return (
+                    <Link
+                      href="/"
+                      key={link.label}
+                      className="flex gap-4 md:flex-col lg:flex-row"
+                    >
+                      <p className="whitespace-nowrap">{link.label}</p>
+                      <p className="medium-14 whitespace-nowrap text-blue-70">
+                        {link.value}
+                      </p>
+                    </Link>
+                  );
+                })}
+              </FooterComp>
+            </div>
+            <div className="flex flex-row gap-5">
+              <FooterComp title={SOCIALS.title}>
+                <ul className="regular-14 flex gap-4 text-gray-30">
+                  {SOCIALS.links.map((link) => {
+                    return (
+                      <Link href="/" key={link}>
+                        <Image src={link} alt="logo" width={24} height={24} />
+                      </Link>
+                    );
+                  })}
+                </ul>
+              </FooterComp>
+            </div>
+          </div>
+        </div>
+        <div className="border bg-gray-20" />
+        <p className="regular-14 w-full text-center text-gray-30">{currentYear} HiLink | All Rights Reserved</p>
+      </div>
+    </footer>
+  );
+};
+
+type FooterProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const FooterComp = ({ title, children }: FooterProps) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h2 className="bold-18 whitespace-nowrap">{title}</h2>
+      {children}
+    </div>
+  );
+};
+
+export default Footer;
